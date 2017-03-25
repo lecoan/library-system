84dbeb2ccd104ba1e0d64805d5075863247d0d6b
lecoan <yangbupt0647@outlook.com>
2017-03-25 10:13:50 +0800

    renove

--- src/App.java
+++ src/App.java
@@ -8,36 +8,6 @@ import service.BookOperate;
 
 import java.io.*;
 
-class BorrowMemory implements Serializable{
-    private String borrowtime;
-    private String borrowman;
-    private String returntime;
-
-    public void setBorrowtime(String borrowtime) {
-        this.borrowtime = borrowtime;
-    }
-
-    public void setBorrowman(String borrowman) {
-        this.borrowman = borrowman;
-    }
-
-    public void setReturntime(String returntime) {
-        this.returntime = returntime;
-    }
-
-    public String getBorrowtime() {
-        return borrowtime;
-    }
-
-    public String getBorrowman() {
-        return borrowman;
-    }
-
-    public String getReturntime() {
-        return returntime;
-    }
-}
-
 public class App {
     public static void main(String args[]) {
         Book test = new Book();
