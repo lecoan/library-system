aeaa01d82be3ae54cbcd3f3c9355d59bff260608
unknown <>
2017-04-07 13:05:53 +0800

    test for remove instance

--- src/controler/UserControler.java
+++ src/controler/UserControler.java
@@ -15,10 +15,8 @@ import java.util.List;
  * Created by ghoskno on 3/29/17.
  */
 public class UserControler {
-//    private List<BookPathTable> curBookList = null;
+    private List<BookPathTable> curBookList = null;
     BookOperate bookOperate = BookOperate.getInstance();
-    FindBookFrame findBookFrame = FindBookFrame.getInstance();
-    CommonControler commonControler = new CommonControler();
     UserView UserPanel = null;
 
     public UserControler() {
@@ -27,15 +25,15 @@ public class UserControler {
             //查找
             @Override
             public void mouseClicked(MouseEvent e) {
-                findBookFrame.showFindBookField();
-                commonControler.findBook();
+                UserPanel.findBookFrame.showFindBookField();
+                findBook();
             }
         });
-        findBookFrame.bookListTable.addMouseListener(new MouseAdapter() {
+        UserPanel.findBookFrame.bookListTable.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 if(e.getClickCount() == 2){
-                    showBookItem(findBookFrame.bookListTable.getSelectedRow());
+                    showBookItem( UserPanel.findBookFrame.bookListTable.getSelectedRow());
                 }
             }
         });
@@ -69,22 +67,97 @@ public class UserControler {
 
             }
         });
-        findBookFrame.findBookByIsbn.addMouseListener(new MouseAdapter() {
+        UserPanel.findBookFrame.initErrAlert();
+    }
+
+
+    private void checkList(List<BookPathTable> extraBookList){
+        if(curBookList != null) {
+            curBookList.retainAll(extraBookList);
+            if(curBookList != null) {
+                UserPanel.findBookFrame.showBookList(curBookList);
+                addConditionLabel( UserPanel.findBookFrame.searchBook.getText());
+            }
+            else
+                UserPanel.findBookFrame.findErrAlert("没有找到符合条件图书！");
+        }
+        else{
+            UserPanel.findBookFrame.showBookList(extraBookList);
+            curBookList = extraBookList;
+            addConditionLabel( UserPanel.findBookFrame.searchBook.getText());
+        }
+    }
+    private void addConditionLabel(String con){
+        UserPanel.findBookFrame.ConditionsLabel.setText( UserPanel.findBookFrame.ConditionsLabel.getText() +"  \"  "+ con + "  \"  ");
+        UserPanel.findBookFrame.ConditionsLabel.invalidate();
+    }
+    private void clearConditionLabel(){
+        UserPanel.findBookFrame.ConditionsLabel.setText("");
+    }
+    private void findBook(){
+        //处理查找书的按钮点击事件
+        UserPanel.findBookFrame.findBookByAuthor.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                Book bookItem = bookOperate.getBookbyIsbn(findBookFrame.searchBook.getText());
+                List<BookPathTable> extraBookList = bookOperate.getBookbyWriter( UserPanel.findBookFrame.searchBook.getText());
+                if (extraBookList != null )
+                    checkList(extraBookList);
+                else
+                    UserPanel.findBookFrame.findErrAlert("【作者：" +  UserPanel.findBookFrame.searchBook.getText() + "】");
+            }
+        });
+        UserPanel.findBookFrame.findBookByName.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                List<BookPathTable> extraBookList = bookOperate.getBookbyName( UserPanel.findBookFrame.searchBook.getText());
+                if (extraBookList != null)
+                    checkList(extraBookList);
+                else
+                    UserPanel.findBookFrame.findErrAlert("【书名：" +  UserPanel.findBookFrame.searchBook.getText() + "】");
+            }
+        });
+        UserPanel.findBookFrame.findBookByKind.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                List<BookPathTable> extraBookList = bookOperate.getBookbyKind( UserPanel.findBookFrame.searchBook.getText());
+                if (extraBookList != null)
+                    checkList(extraBookList);
+                else
+                    UserPanel.findBookFrame.findErrAlert("【种类：" +  UserPanel.findBookFrame.searchBook.getText() + "】");
+            }
+        });
+        UserPanel.findBookFrame.findBookByPublisher.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                List<BookPathTable> extraBookList = bookOperate.getBookbyPublisher( UserPanel.findBookFrame.searchBook.getText());
+                if (extraBookList != null)
+                    checkList(extraBookList);
+                else
+                    UserPanel.findBookFrame.findErrAlert("【出版社：" +  UserPanel.findBookFrame.searchBook.getText() + "】");
+            }
+        });
+        UserPanel.findBookFrame.findBookByIsbn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Book bookItem = bookOperate.getBookbyIsbn( UserPanel.findBookFrame.searchBook.getText());
                 if (bookItem != null) {
-                    commonControler.curBookList = null;
+                    curBookList = null;
 //                    System.out.print(bookItem);
                     showBookItem(bookItem);
                 }
                 else
-                    findBookFrame.findErrAlert("【ISBN ： " + findBookFrame.searchBook.getText() + "】");
+                    UserPanel.findBookFrame.findErrAlert("【ISBN ： " +  UserPanel.findBookFrame.searchBook.getText() + "】");
             }
         });
-        findBookFrame.initErrAlert();
+        UserPanel.findBookFrame.clearBookBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                curBookList = null;
+                UserPanel.findBookFrame.showBookList(curBookList);
+                clearConditionLabel();
+            }
+        });
     }
-    
 
     private void showBookItem(Book bookItem){
     //    显示搜索到的单本书
@@ -93,7 +166,7 @@ public class UserControler {
     }
 
     private void showBookItem(int row){
-        showBookItem(bookOperate.getBookbyIsbn(commonControler.curBookList.get(row).getIsbn()));
+        showBookItem(bookOperate.getBookbyIsbn(curBookList.get(row).getIsbn()));
     }
 
     public void jieyueRetrun(){}
@@ -101,7 +174,9 @@ public class UserControler {
 
 
     public static void main(String[] args){
+
         UserControler test = new UserControler();
+        UserControler test2 = new UserControler();
     }
 
 
