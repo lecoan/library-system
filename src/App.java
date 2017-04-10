df6a0e0c8c61e450e6d47130d03674123092a108
lecoan <yangbupt0647@outlook.com>
2017-04-10 13:38:25 +0800

    i dont konw

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import bean.Customer;
 import service.CustomerService;
 import view.RegisterView;
+import view.StartUpView;
 
 /**
  * Created by lecoan on 17-3-1.
@@ -8,6 +9,6 @@ import view.RegisterView;
 
 public class App {
     public static void main(String args[]) {
-
+        StartUpView view = new StartUpView();
     }
 }
